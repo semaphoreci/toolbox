e6e0864e95d8c9d975b472b03e2a31a4b9c0bebd
Igor Šarčević <igor@renderedtext.com>
2020-12-22 14:20:02 +0100

    TDD driven release process creation

--- release/create_release.sh
+++ release/create_release.sh
@@ -5,30 +5,51 @@ set -euo pipefail
 ARTIFACT_CLI_VERSION="v0.2.8"
 ARTIFACT_CLI_URL="https://github.com/semaphoreci/artifact/releases/download/$ARTIFACT_CLI_VERSION"
 
+#
+# Create release dirs
+#
+echo "Create initial content of the release directories"
+
+rm -rf /tmp/Linux
+rm -rf /tmp/Darwin
+
 mkdir -p /tmp/Linux/toolbox
 mkdir -p /tmp/Darwin/toolbox
 
-#
-# Get artifact releases
-#
-curl -s -L --retry 5 $ARTIFACT_CLI_URL/artifact_Linux_x86_64.tar.gz -o /tmp/Linux/Linux.tar.gz
-curl -s -L --retry 5 $ARTIFACT_CLI_URL/artifact_Darwin_x86_64.tar.gz -o /tmp/Darwin/Darwin.tar.gz
+cp -R ~/$SEMAPHORE_GIT_DIR/* /tmp/Linux/toolbox
+cp -R ~/$SEMAPHORE_GIT_DIR/* /tmp/Darwin/toolbox
+
+rm -rf /tmp/Linux/toolbox/.git
+rm -rf /tmp/Darwin/toolbox/.git
+
+rm -rf /tmp/Linux/toolbox/.gitignore
+rm -rf /tmp/Darwin/toolbox/.gitignore
+
+rm -rf /tmp/Linux/toolbox/Makefile
+rm -rf /tmp/Darwin/toolbox/Makefile
+
+rm -rf /tmp/Linux/toolbox/release
+rm -rf /tmp/Darwin/toolbox/release
+
+rm -rf /tmp/Linux/toolbox/tests
+rm -rf /tmp/Darwin/toolbox/tests
 
 #
-# Unpack artifacts
+# Download and add artifact CLI to the release
 #
-cd /tmp/Linux && tar -zxf Linux.tar.gz && mv artifact toolbox/ && cd -
-cd /tmp/Darwin && tar -zxf Darwin.tar.gz && mv artifact toolbox/ && cd -
+echo "Download Artifact CLI"
 
-#
-# Cp toolbox files, to not mess up workspace
-#
-cp ~/$SEMAPHORE_GIT_DIR/* /tmp/Linux/toolbox
-cp ~/$SEMAPHORE_GIT_DIR/* /tmp/Darwin/toolbox
+curl -s -L --retry 5 $ARTIFACT_CLI_URL/artifact_Linux_x86_64.tar.gz -o /tmp/Linux/artifact_Linux.tar.gz
+curl -s -L --retry 5 $ARTIFACT_CLI_URL/artifact_Darwin_x86_64.tar.gz -o /tmp/Darwin/artifact_Darwin.tar.gz
+
+cd /tmp/Linux && tar -zxf artifact_Linux.tar.gz && mv artifact toolbox/ && cd -
+cd /tmp/Darwin && tar -zxf artifact_Darwin.tar.gz && mv artifact toolbox/ && cd -
 
 #
 # Create linux release
 #
+echo "Creating linux.tar"
+
 cd /tmp/Linux
 tar -cf linux.tar toolbox
 
@@ -38,6 +59,8 @@ tar --list --verbose --file=linux.tar
 #
 # Create darwin release
 #
+echo "Creating darwin.tar"
+
 cd /tmp/Darwin
 tar -cf darwin.tar toolbox
 
@@ -47,6 +70,9 @@ tar --list --verbose --file=darwin.tar
 #
 # Upload created release files to GitHub
 #
+
+echo "Uploading to GitHub"
+
 curl \
   -X POST \
   -H "Authorization: token $GITHUB_TOKEN" \
@@ -75,3 +101,6 @@ curl \
     "https://uploads.github.com/repos/semaphoreci/toolbox/releases/$release_id/assets?name=darwin.tar"
 
 echo "darwin.tar uploaded"
+
+echo "Everything DONE"
+echo "🍻"
