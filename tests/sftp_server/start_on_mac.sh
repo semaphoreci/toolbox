d49f6dcdf7d5dd7493b6b4b304d26d832fd548e8
Igor Šarčević <igor@renderedtext.com>
2021-01-21 13:21:24 +0100

    Start sftp server on MacOS

--- tests/sftp_server/start_on_mac.sh
+++ tests/sftp_server/start_on_mac.sh
@@ -1,4 +1,12 @@
-sudo sh -c 'echo "Subsystem sftp internal-sftp" >> /private/etc/ssh/sshd_config"'
+sudo sh -c 'echo "Subsystem sftp internal-sftp" >> /private/etc/ssh/sshd_config'
 
 sudo launchctl stop com.openssh.sshd
 sudo launchctl start com.openssh.sshd
+
+cp tests/sftp_server/id_rsa ~/.ssh/semaphore_cache_key
+chmod 0600 ~/.ssh/semaphore_cache_key
+
+ssh-keyscan -p 22 -H localhost >> ~/.ssh/known_hosts
+
+export SEMAPHORE_CACHE_URL=localhost:22
+export SEMAPHORE_CACHE_USERNAME=semaphore
