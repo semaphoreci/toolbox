e72f89102ff078caa6debb1a55f9767d514de2f3
Nick Kutryj <mikolaj.kutryj@gmail.com>
2021-04-15 13:43:13 +0200

    Bump TR CLI version to 0.0.5

--- release/create.sh
+++ release/create.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ARTIFACT_CLI_VERSION="v0.4.4"
 WHEN_CLI_VERSION="v1.0.5"
 SPC_CLI_VERSION="v1.6.0"
-TEST_RESULTS_CLI_VERSION="v0.0.4"
+TEST_RESULTS_CLI_VERSION="v0.0.5"
 
 ARTIFACT_CLI_URL="https://github.com/semaphoreci/artifact/releases/download/$ARTIFACT_CLI_VERSION"
 SPC_CLI_URL="https://github.com/semaphoreci/spc/releases/download/$SPC_CLI_VERSION"
