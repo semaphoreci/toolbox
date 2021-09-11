c78e347af27c3293ccc5213776bca59377fc8f08
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2021-09-11 10:51:52 -0300

    More lookup store tests

--- cache-cli/pkg/files/lookup_test.go
+++ cache-cli/pkg/files/lookup_test.go
@@ -31,8 +31,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum(".nvmrc")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, ".nvmrc", []LookupResultEntry{
-			{Path: fmt.Sprintf("%s/.nvm", homedir), Keys: []string{fmt.Sprintf("nvm-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: ".nvmrc",
+				Entries: []LookupResultEntry{
+					{Path: fmt.Sprintf("%s/.nvm", homedir), Keys: []string{fmt.Sprintf("nvm-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -43,8 +48,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("Gemfile.lock")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "Gemfile.lock", []LookupResultEntry{
-			{Path: "vendor/bundle", Keys: []string{fmt.Sprintf("gems-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "Gemfile.lock",
+				Entries: []LookupResultEntry{
+					{Path: "vendor/bundle", Keys: []string{fmt.Sprintf("gems-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -55,8 +65,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("package-lock.json")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "package-lock.json", []LookupResultEntry{
-			{Path: "node_modules", Keys: []string{fmt.Sprintf("node-modules-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "package-lock.json",
+				Entries: []LookupResultEntry{
+					{Path: "node_modules", Keys: []string{fmt.Sprintf("node-modules-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -67,8 +82,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("requirements.txt")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "requirements.txt", []LookupResultEntry{
-			{Path: ".pip_cache", Keys: []string{fmt.Sprintf("requirements-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "requirements.txt",
+				Entries: []LookupResultEntry{
+					{Path: ".pip_cache", Keys: []string{fmt.Sprintf("requirements-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -79,8 +99,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("composer.lock")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "composer.lock", []LookupResultEntry{
-			{Path: "vendor", Keys: []string{fmt.Sprintf("composer-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "composer.lock",
+				Entries: []LookupResultEntry{
+					{Path: "vendor", Keys: []string{fmt.Sprintf("composer-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -91,8 +116,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("Podfile.lock")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "Podfile.lock", []LookupResultEntry{
-			{Path: "Pods", Keys: []string{fmt.Sprintf("pods-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "Podfile.lock",
+				Entries: []LookupResultEntry{
+					{Path: "Pods", Keys: []string{fmt.Sprintf("pods-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -103,8 +133,13 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("go.sum")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "go.sum", []LookupResultEntry{
-			{Path: fmt.Sprintf("%s/go/pkg/mod", homedir), Keys: []string{fmt.Sprintf("go-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "go.sum",
+				Entries: []LookupResultEntry{
+					{Path: fmt.Sprintf("%s/go/pkg/mod", homedir), Keys: []string{fmt.Sprintf("go-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -115,9 +150,14 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("yarn.lock")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "yarn.lock", []LookupResultEntry{
-			{Path: fmt.Sprintf("%s/.cache/yarn", homedir), Keys: []string{fmt.Sprintf("yarn-cache-master-%s", checksum)}},
-			{Path: "node_modules", Keys: []string{fmt.Sprintf("node-modules-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "yarn.lock",
+				Entries: []LookupResultEntry{
+					{Path: fmt.Sprintf("%s/.cache/yarn", homedir), Keys: []string{fmt.Sprintf("yarn-cache-master-%s", checksum)}},
+					{Path: "node_modules", Keys: []string{fmt.Sprintf("node-modules-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -128,9 +168,14 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("mix.lock")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "mix.lock", []LookupResultEntry{
-			{Path: "deps", Keys: []string{fmt.Sprintf("mix-deps-master-%s", checksum)}},
-			{Path: "_build", Keys: []string{fmt.Sprintf("mix-build-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "mix.lock",
+				Entries: []LookupResultEntry{
+					{Path: "deps", Keys: []string{fmt.Sprintf("mix-deps-master-%s", checksum)}},
+					{Path: "_build", Keys: []string{fmt.Sprintf("mix-build-master-%s", checksum)}},
+				},
+			},
 		})
 	})
 
@@ -141,23 +186,56 @@ func Test__LookupStore(t *testing.T) {
 		checksum, err := generateChecksum("pom.xml")
 		assert.Nil(t, err)
 
-		assertLookupStoreResult(t, "pom.xml", []LookupResultEntry{
-			{Path: ".m2", Keys: []string{fmt.Sprintf("maven-master-%s", checksum)}},
-			{Path: "target", Keys: []string{fmt.Sprintf("maven-target-master-%s", checksum)}},
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "pom.xml",
+				Entries: []LookupResultEntry{
+					{Path: ".m2", Keys: []string{fmt.Sprintf("maven-master-%s", checksum)}},
+					{Path: "target", Keys: []string{fmt.Sprintf("maven-target-master-%s", checksum)}},
+				},
+			},
+		})
+	})
+
+	t.Run("finds requirements.txt and package-lock.json", func(t *testing.T) {
+		err := os.Chdir(fmt.Sprintf("%s/test/autocache/multiple-files", rootPath))
+		assert.Nil(t, err)
+
+		requirementsChecksum, err := generateChecksum("requirements.txt")
+		assert.Nil(t, err)
+
+		packageLockChecksum, err := generateChecksum("package-lock.json")
+		assert.Nil(t, err)
+
+		assertLookupStoreResults(t, []LookupResult{
+			{
+				DetectedFile: "package-lock.json",
+				Entries: []LookupResultEntry{
+					{Path: "node_modules", Keys: []string{fmt.Sprintf("node-modules-master-%s", packageLockChecksum)}},
+				},
+			},
+			{
+				DetectedFile: "requirements.txt",
+				Entries: []LookupResultEntry{
+					{Path: ".pip_cache", Keys: []string{fmt.Sprintf("requirements-master-%s", requirementsChecksum)}},
+				},
+			},
 		})
 	})
 }
 
-func assertLookupStoreResult(t *testing.T, detectedFile string, entries []LookupResultEntry) {
+func assertLookupStoreResults(t *testing.T, expectedResults []LookupResult) {
 	results := LookupStore()
-	if assert.Len(t, results, 1) {
-		result := results[0]
-		assert.Equal(t, result.DetectedFile, detectedFile)
-		if assert.Len(t, result.Entries, len(entries)) {
-			for index, entry := range result.Entries {
-				expectedEntry := entries[index]
-				assert.Equal(t, entry.Path, expectedEntry.Path)
-				assert.Equal(t, entry.Keys, expectedEntry.Keys)
+	if assert.Len(t, results, len(expectedResults)) {
+		for resultIndex, result := range results {
+			expectedResult := expectedResults[resultIndex]
+			assert.Equal(t, result.DetectedFile, expectedResult.DetectedFile)
+			if assert.Len(t, result.Entries, len(expectedResult.Entries)) {
+				for index, entry := range result.Entries {
+					expectedEntry := expectedResult.Entries[index]
+					assert.Equal(t, entry.Path, expectedEntry.Path)
+					assert.Equal(t, entry.Keys, expectedEntry.Keys)
+				}
 			}
 		}
 	}
