d7ec770b37baad03308f810980ff3e8185d57e07
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2021-09-15 14:49:23 -0300

    Add restore

--- cache-cli/pkg/storage/sftp_restore.go
+++ cache-cli/pkg/storage/sftp_restore.go
@@ -1,8 +1,29 @@
 package storage
 
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func (s *SFTPStorage) Restore(key string) (*os.File, error) {
-	// TODO
-	return nil, nil
+	localFile, err := ioutil.TempFile("/tmp", fmt.Sprintf("%s-*", key))
+	if err != nil {
+		return nil, err
+	}
+
+	defer localFile.Close()
+
+	remoteFile, err := s.Client.Open(key)
+	if err != nil {
+		os.Remove(localFile.Name())
+		return nil, err
+	}
+
+	_, err = localFile.ReadFrom(remoteFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return localFile, nil
 }
