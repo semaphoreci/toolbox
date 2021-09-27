839c83c8ce0eae4120b02d26194f2c13fd9e587d
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2021-09-27 11:16:03 -0300

    Create self-hosted tarballs as well

--- release/create.sh
+++ release/create.sh
@@ -12,93 +12,101 @@ SPC_CLI_URL="https://github.com/semaphoreci/spc/releases/download/$SPC_CLI_VERSI
 WHEN_CLI_URL="https://github.com/renderedtext/when/releases/download/$WHEN_CLI_VERSION"
 TEST_RESULTS_CLI_URL="https://github.com/semaphoreci/test-results/releases/download/$TEST_RESULTS_CLI_VERSION"
 
-#
-# Create release dirs
-#
-echo "Create initial content of the release directories"
+create_tarball() {
+  tarball_name=$1
+  path=$2
 
-rm -rf /tmp/Linux
-rm -rf /tmp/Darwin
+  echo "Creating ${tarball_name}..."
+  cd ${path}
+  tar -cf ${tarball_name} toolbox
 
-mkdir -p /tmp/Linux/toolbox
-mkdir -p /tmp/Darwin/toolbox
+  echo "${tarball_name} created. Contents: "
+  tar --list --verbose --file=${tarball_name}
+}
 
-cp -R ~/$SEMAPHORE_GIT_DIR/* /tmp/Linux/toolbox
-cp -R ~/$SEMAPHORE_GIT_DIR/* /tmp/Darwin/toolbox
+include_external_linux_binary() {
+  url=$1
+  binary_name=$2
+  destination_path=$3
 
-rm -rf /tmp/Linux/toolbox/.git
-rm -rf /tmp/Darwin/toolbox/.git
+  echo "Downloading ${binary_name} for linux..."
+  curl -s -L --fail --retry 5 ${url}/${binary_name}_Linux_x86_64.tar.gz -o ${destination_path}/${binary_name}_Linux.tar.gz
+  cd ${destination_path} && tar -zxf ${binary_name}_Linux.tar.gz && mv ${binary_name} toolbox/ && cd - > /dev/null
+}
 
-rm -rf /tmp/Linux/toolbox/.gitignore
-rm -rf /tmp/Darwin/toolbox/.gitignore
+include_external_darwin_binary() {
+  url=$1
+  binary_name=$2
+  destination_path=$3
 
-rm -rf /tmp/Linux/toolbox/Makefile
-rm -rf /tmp/Darwin/toolbox/Makefile
+  echo "Downloading ${binary_name} for darwin..."
+  curl -s -L --fail --retry 5 ${url}/${binary_name}_Darwin_x86_64.tar.gz -o ${destination_path}/${binary_name}_Darwin.tar.gz
+  cd ${destination_path} && tar -zxf ${binary_name}_Darwin.tar.gz && mv ${binary_name} toolbox/ && cd - > /dev/null
+}
 
-rm -rf /tmp/Linux/toolbox/release
-rm -rf /tmp/Darwin/toolbox/release
+hosted::create_initial_content() {
+  echo "Creating initial content of the release directories for the hosted toolbox..."
 
-rm -rf /tmp/Linux/toolbox/tests
-rm -rf /tmp/Darwin/toolbox/tests
+  rm -rf /tmp/Linux
+  rm -rf /tmp/Darwin
 
-#
-# Download and add artifact CLI to the release
-#
-echo "Download Artifact CLI"
+  mkdir -p /tmp/Linux/toolbox
+  mkdir -p /tmp/Darwin/toolbox
 
-curl -s -L --fail --retry 5 $ARTIFACT_CLI_URL/artifact_Linux_x86_64.tar.gz -o /tmp/Linux/artifact_Linux.tar.gz
-curl -s -L --fail --retry 5 $ARTIFACT_CLI_URL/artifact_Darwin_x86_64.tar.gz -o /tmp/Darwin/artifact_Darwin.tar.gz
+  cp -R ~/$SEMAPHORE_GIT_DIR/* /tmp/Linux/toolbox
+  cp -R ~/$SEMAPHORE_GIT_DIR/* /tmp/Darwin/toolbox
 
-cd /tmp/Linux && tar -zxf artifact_Linux.tar.gz && mv artifact toolbox/ && cd -
-cd /tmp/Darwin && tar -zxf artifact_Darwin.tar.gz && mv artifact toolbox/ && cd -
+  exclusions=(.git .gitignore Makefile release tests cache-cli install-self-hosted-toolbox self-hosted-toolbox)
+  for exclusion in "${exclusions[@]}"; do
+    rm -rf /tmp/Linux/toolbox/${exclusion}
+    rm -rf /tmp/Darwin/toolbox/${exclusion}
+  done
+}
 
-#
-# Download and add When CLI to the release
-#
-echo "Download When CLI"
+self_hosted::create_initial_content() {
+  echo "Creating initial content of the release directories for the self-hosted toolbox..."
 
-curl -s -L --retry 5 $WHEN_CLI_URL/when -o /tmp/Linux/toolbox/when
+  rm -rf /tmp/self-hosted-Linux
+  rm -rf /tmp/self-hosted-Darwin
 
-chmod +x /tmp/Linux/toolbox/when
+  mkdir -p /tmp/self-hosted-Linux/toolbox
+  mkdir -p /tmp/self-hosted-Darwin/toolbox
 
-#
-# Download and add SPC CLI to the release
-#
-echo "Download SPC CLI"
+  cp ~/$SEMAPHORE_GIT_DIR/install-self-hosted-toolbox /tmp/self-hosted-Linux/toolbox/install-toolbox
+  cp ~/$SEMAPHORE_GIT_DIR/install-self-hosted-toolbox /tmp/self-hosted-Darwin/toolbox/install-toolbox
+  cp ~/$SEMAPHORE_GIT_DIR/self-hosted-toolbox /tmp/self-hosted-Linux/toolbox/toolbox
+  cp ~/$SEMAPHORE_GIT_DIR/self-hosted-toolbox /tmp/self-hosted-Darwin/toolbox/toolbox
+  cp ~/$SEMAPHORE_GIT_DIR/libcheckout /tmp/self-hosted-Linux/toolbox/
+  cp ~/$SEMAPHORE_GIT_DIR/libcheckout /tmp/self-hosted-Darwin/toolbox/
+}
 
-curl -s -L --fail --retry 5 $SPC_CLI_URL/spc_Linux_x86_64.tar.gz -o /tmp/Linux/spc_Linux.tar.gz
+self_hosted::pack() {
+  self_hosted::create_initial_content
+  include_external_linux_binary $ARTIFACT_CLI_URL "artifact" /tmp/self-hosted-Linux
+  include_external_darwin_binary $ARTIFACT_CLI_URL "artifact" /tmp/self-hosted-Darwin
+  include_external_linux_binary $TEST_RESULTS_CLI_URL "test-results" /tmp/self-hosted-Linux
+  include_external_darwin_binary $TEST_RESULTS_CLI_URL "test-results" /tmp/self-hosted-Darwin
+  cd ~/$SEMAPHORE_GIT_DIR/cache-cli && make build OS=linux && mv bin/cache /tmp/self-hosted-Linux/toolbox && cd - > /dev/null
+  cd ~/$SEMAPHORE_GIT_DIR/cache-cli && make build OS=darwin && mv bin/cache /tmp/self-hosted-Darwin/toolbox && cd - > /dev/null
+}
 
-cd /tmp/Linux && tar -zxf spc_Linux.tar.gz && mv spc toolbox/ && cd -
+hosted::pack() {
+  hosted::create_initial_content
+  include_external_linux_binary $ARTIFACT_CLI_URL "artifact" /tmp/Linux
+  include_external_darwin_binary $ARTIFACT_CLI_URL "artifact" /tmp/Darwin
+  include_external_linux_binary $TEST_RESULTS_CLI_URL "test-results" /tmp/Linux
+  include_external_darwin_binary $TEST_RESULTS_CLI_URL "test-results" /tmp/Darwin
+  include_external_linux_binary $SPC_CLI_URL "spc" /tmp/Linux
 
-#
-# Download and add Test Results CLI to the release
-#
-echo "Download Test Results CLI"
+  echo "Downloading when CLI..."
+  curl -s -L --retry 5 $WHEN_CLI_URL/when -o /tmp/Linux/toolbox/when
+  chmod +x /tmp/Linux/toolbox/when
+}
 
-curl -s -L --fail --retry 5 $TEST_RESULTS_CLI_URL/test-results_Linux_x86_64.tar.gz -o /tmp/Linux/test-results_Linux.tar.gz
-curl -s -L --fail --retry 5 $TEST_RESULTS_CLI_URL/test-results_Darwin_x86_64.tar.gz -o /tmp/Darwin/test-results_Darwin.tar.gz
+hosted::pack
+create_tarball "linux.tar" /tmp/Linux
+create_tarball "darwin.tar" /tmp/Darwin
 
-cd /tmp/Linux && tar -zxf test-results_Linux.tar.gz && mv test-results toolbox/ && cd -
-cd /tmp/Darwin && tar -zxf test-results_Darwin.tar.gz && mv test-results toolbox/ && cd -
-
-#
-# Create linux release
-#
-echo "Creating linux.tar"
-
-cd /tmp/Linux
-tar -cf linux.tar toolbox
-
-echo "toolbox Linux content: "
-tar --list --verbose --file=linux.tar
-
-#
-# Create mac release
-#
-echo "Creating darwin.tar"
-
-cd /tmp/Darwin
-tar -cf darwin.tar toolbox
-
-echo "toolbox Darwin content: "
-tar --list --verbose --file=darwin.tar
+self_hosted::pack
+create_tarball "linux.tar" /tmp/self-hosted-Linux
+create_tarball "darwin.tar" /tmp/self-hosted-Darwin
--- release/upload.sh
+++ release/upload.sh
@@ -9,6 +9,8 @@ set -euo pipefail
 #   1. Create the tarballs by running release/create.sh, this will create:
 #      - /tmp/Linux/linux.tar
 #      - /tmp/Darwin/darwin.tar
+#      - /tmp/self-hosted-Linux/linux.tar
+#      - /tmp/self-hosted-Darwin/darwin.tar
 #
 #   2. Upload the tarballs to Github by running release/upload.sh.
 #
@@ -38,11 +40,31 @@ curl \
     -X POST \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type: $(file -b --mime-type /tmp/Linux/self-hosted-linux.tar)" \
+    --data-binary @/tmp/self-hosted-Linux/linux.tar \
+    "https://uploads.github.com/repos/semaphoreci/toolbox/releases/$release_id/assets?name=self-hosted-linux.tar"
+
+echo "self-hosted-linux.tar uploaded"
+
+curl \
+    -X POST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type /tmp/Darwin/darwin.tar)" \
     --data-binary @/tmp/Darwin/darwin.tar \
     "https://uploads.github.com/repos/semaphoreci/toolbox/releases/$release_id/assets?name=darwin.tar"
 
 echo "darwin.tar uploaded"
+
+curl \
+    -X POST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type: $(file -b --mime-type /tmp/Darwin/self-hosted-darwin.tar)" \
+    --data-binary @/tmp/self-hosted-Darwin/darwin.tar \
+    "https://uploads.github.com/repos/semaphoreci/toolbox/releases/$release_id/assets?name=self-hosted-darwin.tar"
+
+echo "self-hosted-darwin.tar uploaded"
 
 echo "Everything DONE"
 echo "🍻"
