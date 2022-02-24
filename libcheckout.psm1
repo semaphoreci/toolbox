37ddc034cbbbd8c2ae1b798f939029ff3e2666e0
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-02-24 16:01:35 -0300

    Require git to be in the PATH

--- libcheckout.psm1
+++ libcheckout.psm1
@@ -1,6 +1,12 @@
 function Checkout {
   $ErrorActionPreference = "Stop"
 
+  Get-Command git > $null
+  if (-not $?) {
+    Write-Output '[ERROR] Could not find git.exe in $env:Path.'
+    Exit 1
+  }
+
   if ($null -eq $env:SEMAPHORE_GIT_BRANCH) {
     Write-Output "[ERROR] SEMAPHORE_GIT_BRANCH is required."
     Exit 1
@@ -33,7 +39,7 @@ function Checkout {
 }
 
 function Check-Revision {
-  C:\"Program Files"\Git\cmd\git.exe rev-list HEAD..$env:SEMAPHORE_GIT_SHA 2> $null
+  git rev-list HEAD..$env:SEMAPHORE_GIT_SHA 2> $null
   if (-not $?) {
     Write-Output "Revision: $env:SEMAPHORE_GIT_SHA} not found .... Exiting"
     return 1
@@ -46,27 +52,27 @@ function Shallow-Checkout() {
   }
 
   Write-Output "Performing shallow clone with depth: $env:SEMAPHORE_GIT_DEPTH"
-  C:\"Program Files"\Git\cmd\git.exe clone --depth $env:SEMAPHORE_GIT_DEPTH -b $env:SEMAPHORE_GIT_BRANCH $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR
+  git clone --depth $env:SEMAPHORE_GIT_DEPTH -b $env:SEMAPHORE_GIT_BRANCH $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR
 
   if (-not $?) {
     Write-Output "Branch not found performing full clone"
-    C:\"Program Files"\Git\cmd\git.exe clone $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR
+    git clone $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR
     cd $env:SEMAPHORE_GIT_DIR
     Check-Revision
     if ($?) {
-      C:\"Program Files"\Git\cmd\git.exe reset --hard $env:SEMAPHORE_GIT_SHA 2> $null
+      git reset --hard $env:SEMAPHORE_GIT_SHA 2> $null
     } else {
       return 1
     }
   } else {
     cd $env:SEMAPHORE_GIT_DIR
-    C:\"Program Files"\Git\cmd\git.exe reset --hard $env:SEMAPHORE_GIT_SHA 2> $null
+    git reset --hard $env:SEMAPHORE_GIT_SHA 2> $null
     if (-not $?) {
       Write-Output "SHA: $env:SEMAPHORE_GIT_SHA not found performing full clone"
-      C:\"Program Files"\Git\cmd\git.exe fetch --unshallow
+      git fetch --unshallow
       Check-Revision
       if ($?) {
-        C:\"Program Files"\Git\cmd\git.exe reset --hard $env:SEMAPHORE_GIT_SHA 2> $null
+        git reset --hard $env:SEMAPHORE_GIT_SHA 2> $null
       } else {
         return 1
       }
@@ -80,27 +86,27 @@ function Ref-Based-Checkout {
   }
 
   if ($env:SEMAPHORE_GIT_REF_TYPE -eq "pull-request") {
-    C:\"Program Files"\Git\cmd\git.exe clone --depth $env:SEMAPHORE_GIT_DEPTH $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR 2> $null
+    git clone --depth $env:SEMAPHORE_GIT_DEPTH $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR 2> $null
     cd $env:SEMAPHORE_GIT_DIR
-    C:\"Program Files"\Git\cmd\git.exe fetch origin +$env:SEMAPHORE_GIT_REF: 2> $null
+    git fetch origin +$env:SEMAPHORE_GIT_REF: 2> $null
     if (-not $?) {
       Write-Output "Revision: $env:SEMAPHORE_GIT_SHA not found .... Exiting"
       return 1
     } else {
-      C:\"Program Files"\Git\cmd\git.exe checkout -qf FETCH_HEAD
+      git checkout -qf FETCH_HEAD
       Write-Output "HEAD is now at $env:SEMAPHORE_GIT_SHA"
       return 0
     }
   }
 
   if ($env:SEMAPHORE_GIT_REF_TYPE -eq "tag") {
-    C:\"Program Files"\Git\cmd\git.exe clone --depth $env:SEMAPHORE_GIT_DEPTH -b $env:SEMAPHORE_GIT_TAG_NAME $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR 2> $null
+    git clone --depth $env:SEMAPHORE_GIT_DEPTH -b $env:SEMAPHORE_GIT_TAG_NAME $env:SEMAPHORE_GIT_URL $env:SEMAPHORE_GIT_DIR 2> $null
     if (-not $?) {
       Write-Output "Release $env:SEMAPHORE_GIT_TAG_NAME not found .... Exiting"
       return 1
     } else {
       cd $env:SEMAPHORE_GIT_DIR
-      C:\"Program Files"\Git\cmd\git.exe checkout -qf $env:SEMAPHORE_GIT_TAG_NAME
+      git checkout -qf $env:SEMAPHORE_GIT_TAG_NAME
       Write-Output "HEAD is now at $env:SEMAPHORE_GIT_SHA Release $env:SEMAPHORE_GIT_TAG_NAME"
       return 0
     }
