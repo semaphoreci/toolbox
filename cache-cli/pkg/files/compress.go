92e757eddcf2936e05c2ae23b14f0baaa13ced99
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-03-25 17:40:51 -0300

    Print error when compress fails

--- cache-cli/pkg/files/compress.go
+++ cache-cli/pkg/files/compress.go
@@ -13,8 +13,10 @@ func Compress(key, path string) (string, error) {
 	tempFileName := filepath.Join(os.TempDir(), fmt.Sprintf("%s-%d", key, epochNanos))
 
 	cmd := compressionCommand(path, tempFileName)
-	_, err := cmd.Output()
+	output, err := cmd.CombinedOutput()
 	if err != nil {
+		fmt.Printf("Error compressing %s: %s\n", path, output)
+		fmt.Printf("Error: %v\n", err)
 		return tempFileName, err
 	}
 
