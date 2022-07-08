1e9d1d9733bf073c983ae8312b3300ba19ae93e5
Igor Šarčević <igor@renderedtext.com>
2022-07-08 11:36:17 +0000

    Define the base code structure

--- sem-vars/cmd/put.go
+++ sem-vars/cmd/put.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var putCmd = &cobra.Command{
+	Use:   "put key=value",
+	Short: "Stores a variable",
+	Run:   RunPutCmd,
+}
+
+func RunPutCmd(cmd *cobra.Command, args []string) {
+	fmt.Printf("putting stuff")
+}
+
+func init() {
+	RootCmd.AddCommand(putCmd)
+}
