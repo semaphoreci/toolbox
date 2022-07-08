c0db063f9e0e42e38a92b7a10bf8755a6b8a19e6
Igor Šarčević <igor@renderedtext.com>
2022-07-08 11:36:17 +0000

    Define the base code structure

--- sem-vars/cmd/get.go
+++ sem-vars/cmd/get.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var getCmd = &cobra.Command{
+	Use:   "get key",
+	Short: "Get a variable",
+	Run:   RunPutCmd,
+}
+
+func RunGetCmd(cmd *cobra.Command, args []string) {
+	fmt.Printf("getting stuff")
+}
+
+func init() {
+	RootCmd.AddCommand(getCmd)
+}
