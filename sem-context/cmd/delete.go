d7159f4733064850dcf54175d90e95c94e6eca46
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2022-07-26 03:52:00 +0200

    Return 1 if deleting key that does not exist

--- sem-context/cmd/delete.go
+++ sem-context/cmd/delete.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/semaphoreci/toolbox/sem-context/pkg/store"
 	"github.com/semaphoreci/toolbox/sem-context/pkg/utils"
 	"github.com/semaphoreci/toolbox/sem-context/pkg/validators"
@@ -16,7 +18,12 @@ var deleteCmd = &cobra.Command{
 func RunDeleteCmd(cmd *cobra.Command, args []string) {
 	utils.CheckError(validators.ValidateGetAndDeleteArguments(args))
 	key := args[0]
-	err := store.Delete(key)
+	value, err := store.Get(key)
+	utils.CheckError(err)
+	if value == "" {
+		utils.CheckError(&utils.Error{ErrorMessage: fmt.Sprintf("Key %s does not exist", key), ExitCode: 1})
+	}
+	err = store.Delete(key)
 	utils.CheckError(err)
 	//TODO some feedback log
 }
