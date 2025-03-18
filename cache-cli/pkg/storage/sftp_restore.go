270ea9ce529509f696da4f116b37acff4610dc0c
Nandor Bors <nbors@renderedtext.com>
2025-03-18 20:25:31 +0200

    add http download method and tests for sftp storage type

--- cache-cli/pkg/storage/sftp_restore.go
+++ cache-cli/pkg/storage/sftp_restore.go
@@ -11,21 +11,21 @@ func (s *SFTPStorage) Restore(key string) (*os.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	remoteFile, err := s.SFTPClient.Open(key)
 	if err != nil {
 		_ = localFile.Close()
 		_ = os.Remove(localFile.Name())
 		return nil, err
 	}
-	
+
 	_, err = localFile.ReadFrom(remoteFile)
 	if err != nil {
 		_ = localFile.Close()
 		_ = remoteFile.Close()
 		return nil, err
 	}
-	
+
 	err = remoteFile.Close()
 	if err != nil {
 		_ = localFile.Close()
